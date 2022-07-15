3a4ee1c993b1c53b83dd9747eba9ff569e12aafc
Christian González <cgonzalez@opennebula.io>
2022-07-15 21:42:13 +0200

    Fix + rework install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
+# Create a link at $2 (link name) pointing to $1 (target). The original content of $2 is stored
+# as $2.orig.
+function link_and_save() {
+    link_name="$2"
+    target="$1"
+
+    if [ -e "$link_name" ]; then
+        mv "$link_name" "$link_name".orig
+    else
+        # Ensure path exists if file is not already in place
+        mkdir -p "$(dirname "$link_name")"
+    fi
+
+    ln -s "$target" "$link_name"
+}
+
 set -e -o pipefail
 
-if [ "$AVOID_PACMAN" != "christian" ]; then
+if [ "$USER" != "christian" ]; then
     echo "The script must be executed as christian user"
+    exit 255
 fi
 
 SCRIPT_PATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
 REPO_PATH="$(dirname "$SCRIPT_PATH")"
 
-while getopts p:y:i:z:c:x:n:o:v: flag
+while getopts pyizcxnov flag
 do
     case "${flag}" in
         p) AVOID_PACMAN="YES";;
@@ -36,7 +53,7 @@ if [ "$AVOID_PACMAN" != "YES" ]; then
                  rofi \
                  code"
 
-    sudo pacman -Syu "$PACMAN_PKGS"
+    sudo pacman -Syu $(echo "$PACMAN_PKGS" | tr -s '[:space:]' ' ')
 
     read -p "Press enter to continue"
 fi
@@ -45,21 +62,24 @@ if [ "$AVOID_YAY" != "YES" ]; then
     YAY_PKGS="google-chrome \
               spotify"
 
-    yay -Syu "$YAY_PKGS"
+    yay -Syu $(echo "$YAY_PKGS" | tr -s '[:space:]' ' ')
 
     read -p "Press enter to continue"
 fi
 
 # Includes any configuration related to the desktop environment
 if [ "$AVOID_I3" != "YES" ]; then
-    ln -s "$REPO_PATH/files/.i3" /home/christian
-    ln -s "$REPO_PATH/files/.i3status.conf" /home/christian/.i3status.conf
+    link_and_save "$REPO_PATH/files/.i3" /home/christian/.i3
+    link_and_save "$REPO_PATH/files/.i3status.conf" /home/christian/.i3status.conf
 
     # rofi configuration files
-    ln -s "$REPO_PATH/files/rofi" /home/christian/.config
+    link_and_save "$REPO_PATH/files/rofi" /home/christian/.config/rofi
 
     # nitrogen configuration files
-    ln -s "$REPO_PATH/files/nitrogen" /home/christian/.config
+    link_and_save "$REPO_PATH/files/nitrogen" /home/christian/.config/nitrogen
+
+    # Switch from alsa to pulse audio
+    install_pulse
 
     read -p "Press enter to continue"
 fi
@@ -69,50 +89,59 @@ if [ "$AVOID_ZSH" != "YES" ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
     if [ ! -e "$REPO_PATH/files/.zsh_history" ]; then
-        echo ".zsh_history not available. Please copy it manually (avoid commiting it as it might contains passwords." 1>&2
+        echo ".zsh_history not available. Please copy it manually into $REPO_PATH/files (avoid commiting it as it might contains passwords)." 1>&2
+        read -p "Press enter to continue"
+
+        if [ -e "$REPO_PATH/files/.zsh_history" ]; then
+            mv -f "$REPO_PATH/files/.zsh_history" /home/christian/.zsh_history
+        fi
     fi
 
-    ln -s "$REPO_PATH/files/.zshrc" /home/christian/.zshrc
+    link_and_save "$REPO_PATH/files/.zshrc" /home/christian/.zshrc
 
-    mkdir -p /home/christian/.config/terminator/config
-    ln -s "$REPO_PATH/files/terminator_config" /home/christian/.config/terminator/config
-
-    # Switch from alsa to pulse audio
-    install_pulse
+    link_and_save "$REPO_PATH/files/terminator_config" /home/christian/.config/terminator/config
 
     read -p "Press enter to continue"
 fi
 
 if [ "$AVOID_CMDS" != "YES" ]; then
     INSTALL_PATH="/usr/bin"
+
     for FILE in $REPO_PATH/commands; do
-        if [ -e "$INSTALL_PATH/$FILE" ]; then
-            echo "$INSTALL_PATH/$FILE already exists" 1>&2
+        NAME="$(basename "$FILE")"
+
+        if [ -e "$INSTALL_PATH/$NAME" ]; then
+            echo "$INSTALL_PATH/$NAME already exists" 1>&2
         fi
 
-        ln -s "$REPO_PATH/files/terminator_config" "/usr/bin/$FILE"
+        sudo ln -s "$FILE" "/usr/bin/$NAME"
     done
 
     read -p "Press enter to continue"
 fi
 
 if [ "$AVOID_X11" != "YES" ]; then
-    mv /etc/X11/xorg.conf.d/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf.orig
-    mv "$REPO_PATH/files/xorg.conf.d/30-touchpad.conf" /etc/X11/xorg.conf.d/30-touchpad.conf
+    sudo mv /etc/X11/xorg.conf.d/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf.orig
+    sudo mv "$REPO_PATH/files/xorg.conf.d/30-touchpad.conf" /etc/X11/xorg.conf.d/30-touchpad.conf
 
     read -p "Press enter to continue"
 fi
 
-if [ "$AVOID_OTHERS" != "YES" ]; then
-    ln -s "$REPO_PATH/files/.vimrc" /home/christian/.vimrc
-    ln -s "$REPO_PATH/files/.warprc" /home/christian/.warprc
-    ln -s "$REPO_PATH/files/.gdbinit" /home/christian/.gdbinit
-fi
-
 if [ "$AVOID_VSCODE" != "YES" ]; then
-    EXTENSIONS="code-spell-checker blame highlight-trailing-white-spaces nord-visual-studio-code shellcheck vscode-yaml"
+    EXTENSIONS="streetsidesoftware.code-spell-checker \
+                waderyan.gitblame \
+                ybaumes.highlight-trailing-white-spaces \
+                arcticicestudio.nord-visual-studio-code \
+                timonwong.shellcheck \
+                redhat.vscode-yaml"
 
     for EXT in $EXTENSIONS; do
-        code --install-extension "$EXT"
+        code --install-extension $(echo "$EXT" | tr -s '[:space:]' ' ')
     done
+fi
+
+if [ "$AVOID_OTHERS" != "YES" ]; then
+    link_and_save "$REPO_PATH/files/.vimrc" /home/christian/.vimrc
+    link_and_save "$REPO_PATH/files/.warprc" /home/christian/.warprc
+    link_and_save "$REPO_PATH/files/.gdbinit" /home/christian/.gdbinit
 fi
